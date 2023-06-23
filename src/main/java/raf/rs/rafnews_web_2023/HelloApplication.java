8c2eff3347b51263986e229455c604d1db5fa8aa
Tadija <tsimic7419rn@raf.rs>
2023-06-23 17:28:20 +0200

    initial commit.

---
+++ src/main/java/raf/rs/rafnews_web_2023/HelloApplication.java
@@ -0,0 +1,9 @@
+package raf.rs.rafnews_web_2023;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
